37568d0e9178c0fc8751bb5aedc2f07689a19ac7
andresbarbaprocorp <abarba@propuestacorporativa.com>
2025-05-11 22:37:21 -0600

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-      module.exports = {
-        content: [
-          "./src/**/*.{js,jsx,ts,tsx}",
-        ],
-        theme: {
-          extend: {},
-        },
-        plugins: [],
-      }
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/scrollbar'),   // 👈 ✅ AÑADE ESTA LÍNEA
+  ],
+}
