542016ecdabfb11957e5265050ad94f134d9aa28
andresbarbaprocorp <abarba@propuestacorporativa.com>
2025-05-12 02:31:12 -0600

    LayoutSidebar.js

--- src/components/LayoutSidebar.js
+++ src/components/LayoutSidebar.js
@@ -1,4 +1,5 @@
 import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 
 const LayoutSidebar = ({ activePage, setActivePage, onAddModule }) => {
   const menuItems = [
@@ -32,8 +33,7 @@ const LayoutSidebar = ({ activePage, setActivePage, onAddModule }) => {
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
         <path d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z" />
       </svg>
-    ) },
-    { id: 'maintenances', label: 'Mantenimientos', icon: (
+    ) },  { id: 'maintenances', label: 'Mantenimientos', icon: (
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
         <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
       </svg>
@@ -64,8 +64,9 @@ const LayoutSidebar = ({ activePage, setActivePage, onAddModule }) => {
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
         <path fillRule="evenodd" d="M10 5a1 1 0 011 1v3h3a1 1 0 110 2h-3v3a1 1 0 11-2 0v-3H6a1 1 0 110-2h3V6a1 1 0 011-1z" clipRule="evenodd" />
       </svg>
-    ) }
+    ) },
   ];
+  
   return (
     <div className="fixed left-0 top-0 h-full w-64 bg-white shadow-lg overflow-y-auto"
       style={{
@@ -95,7 +96,7 @@ const LayoutSidebar = ({ activePage, setActivePage, onAddModule }) => {
                 onClick={() =>
                   item.id === 'addModule'
                     ? onAddModule && onAddModule()
-                    : setActivePage(item.id)
+                    : setActivePage && setActivePage(item.id)
                 }
                 className={`flex items-center w-full px-4 py-3 text-left transition-colors ${
                   activePage === item.id && item.id !== 'addModule'
@@ -124,6 +125,12 @@ const LayoutSidebar = ({ activePage, setActivePage, onAddModule }) => {
       </div>
     </div>
   );
+};
+
+LayoutSidebar.propTypes = {
+  activePage: PropTypes.string,
+  setActivePage: PropTypes.func,
+  onAddModule: PropTypes.func
 };
 
 export default LayoutSidebar;
